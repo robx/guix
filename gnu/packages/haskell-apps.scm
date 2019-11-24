9010b176c73d1ed7c7c719b803d0245110c932a7
Robert Vollmert <rob@vllmrt.net>
2019-11-24 22:32:26 +0100

    gnu: Add postgrest.
    * gnu/packages/haskell-apps.scm (postgrest): New variable.

--- gnu/packages/haskell-apps.scm
+++ gnu/packages/haskell-apps.scm
@@ -724,3 +724,70 @@ advanced user's otherwise working script to fail under future circumstances.
 code in a file, just clean up import statements and a few other tedious
 items.  This tool tries to help where necessary without getting in the way.")
     (license license:bsd-3)))
+
+(define-public postgrest
+  (package
+    (name "postgrest")
+    (version "6.0.2")
+    (source
+     (origin
+       (method url-fetch)
+       (uri (string-append
+             "https://hackage.haskell.org/package/postgrest/postgrest-"
+             version
+             ".tar.gz"))
+       (sha256
+        (base32
+         "0c1yapjwsccqmj6jh8bkgv15p83dh7bd7ib68cd80pi3n9dplqvw"))))
+    (build-system haskell-build-system)
+    (arguments `(#:tests? #f)) ; tests require a running postgresql server
+    (inputs
+     `(("ghc-aeson" ,ghc-aeson)
+       ("ghc-ansi-wl-pprint" ,ghc-ansi-wl-pprint)
+       ("ghc-base64-bytestring" ,ghc-base64-bytestring)
+       ("ghc-case-insensitive" ,ghc-case-insensitive)
+       ("ghc-cassava" ,ghc-cassava)
+       ("ghc-configurator-pg" ,ghc-configurator-pg)
+       ("ghc-contravariant" ,ghc-contravariant)
+       ("ghc-contravariant-extras"
+        ,ghc-contravariant-extras)
+       ("ghc-either" ,ghc-either)
+       ("ghc-gitrev" ,ghc-gitrev)
+       ("ghc-hasql" ,ghc-hasql)
+       ("ghc-hasql-pool" ,ghc-hasql-pool)
+       ("ghc-hasql-transaction" ,ghc-hasql-transaction)
+       ("ghc-heredoc" ,ghc-heredoc)
+       ("ghc-http" ,ghc-http)
+       ("ghc-http-types" ,ghc-http-types)
+       ("ghc-insert-ordered-containers"
+        ,ghc-insert-ordered-containers)
+       ("ghc-interpolatedstring-perl6"
+        ,ghc-interpolatedstring-perl6)
+       ("ghc-jose" ,ghc-jose)
+       ("ghc-lens" ,ghc-lens)
+       ("ghc-lens-aeson" ,ghc-lens-aeson)
+       ("ghc-network-uri" ,ghc-network-uri)
+       ("ghc-optparse-applicative"
+        ,ghc-optparse-applicative)
+       ("ghc-protolude" ,ghc-protolude)
+       ("ghc-ranged-sets" ,ghc-ranged-sets)
+       ("ghc-regex-tdfa" ,ghc-regex-tdfa)
+       ("ghc-scientific" ,ghc-scientific)
+       ("ghc-swagger2" ,ghc-swagger2)
+       ("ghc-unordered-containers"
+        ,ghc-unordered-containers)
+       ("ghc-vector" ,ghc-vector)
+       ("ghc-wai" ,ghc-wai)
+       ("ghc-wai-cors" ,ghc-wai-cors)
+       ("ghc-wai-extra" ,ghc-wai-extra)
+       ("ghc-wai-middleware-static"
+        ,ghc-wai-middleware-static)
+       ("ghc-cookie" ,ghc-cookie)
+       ("ghc-auto-update" ,ghc-auto-update)
+       ("ghc-warp" ,ghc-warp)
+       ("ghc-retry" ,ghc-retry)))
+    (home-page "https://postgrest.org")
+    (synopsis "REST API for any Postgres database")
+    (description
+     "Reads the schema of a PostgreSQL database and creates RESTful routes for the tables and views, supporting all HTTP verbs that security permits.")
+    (license license:expat)))
